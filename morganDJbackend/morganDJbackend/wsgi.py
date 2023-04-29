bd62d07ca614e1548a5cc788a3abb05ff487a991
EthanHacks8131 <132111789+EthanHacks8131@users.noreply.github.com>
2023-04-29 14:13:38 -0400

    adding DJ backend

---
+++ morganDJbackend/morganDJbackend/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for morganDJbackend project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'morganDJbackend.settings')
+
+application = get_wsgi_application()
